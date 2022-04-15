d5dcc7b0f1c92bf3f16b08308c059726c7c4275a
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-04-15 16:03:55 -0500

    Trending slider finished

--- src/app/components/Cards/CardTrending.jsx
+++ src/app/components/Cards/CardTrending.jsx
@@ -4,24 +4,29 @@ import Icons from "../Icons";
 
 const CardTrending = () => {
   return (
-    <div className="cardT">
-      <img className="cardT_image" src={Img} alt="card image" />
-      <div className="cardT_bookmark">
-        <img src={Icons.BookmarkEmpty} alt="Bookmark" />
-      </div>
-      <div className="cardT_info">
-        <div className="card_data">
-          <p className="card_text">2019</p>
-          <div className="card_category">
-            <img src={Icons.NavMovies} alt="Icon" />
-            <p className="card_text">Movie</p>
-          </div>
-          <p className="card_text">PG</p>
+    <>
+      <div className="cardT">
+        <img className="cardT_image" src={Img} alt="card image" />
+        <div className="cardT_bookmark">
+          <img src={Icons.BookmarkEmpty} alt="Bookmark" />
         </div>
-
-        <p className="card_title">Beyond Earth</p>
+        <div className="cardT_info">
+          <div className="card_data">
+            <p className="card_text">2019</p>
+            <div className="card_category">
+              <img className="card_image" src={Icons.NavMovies} alt="Icon" />
+              <p className="card_text">Movie</p>
+            </div>
+            <p className="card_text">PG</p>
+          </div>
+          <p className="card_title">Beyond Earth</p>
+        </div>
+        <div className="cardT_button">
+          <img src={Icons.IconPlay} alt="Icon play" />
+          <p>Play</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardTrending from "../components/Cards/CardTrending";
 import Layout from "../layout/Layout";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Home = () => {
+  let slides = 1;
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+  const updateDimensions = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  if (windowWidth > 550 && windowWidth < 1024) {
+    slides = 2;
+  }
+  if (windowWidth >= 1024) {
+    slides = 3;
+  }
+
   return (
     <Layout>
       <div className="home">
         <h2 className="home_title">Trending</h2>
         <div className="home_cards">
-          <CardTrending />
-          <CardTrending />
-          <CardTrending />
-          <CardTrending />
-          <CardTrending />
+          <Swiper
+            slidesPerView={slides}
+            spaceBetween={40}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            className="home_swipe"
+          >
+            <SwiperSlide>
+              <CardTrending />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTrending />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTrending />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTrending />
+            </SwiperSlide>
+            <SwiperSlide>
+              <CardTrending />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </Layout>
