8fdcf2385a0f33fa4f9b553ac45df64733f35228
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-04-15 19:48:57 -0500

    Icons card util

--- src/app/components/Cards/CardTrending.jsx
+++ src/app/components/Cards/CardTrending.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import Img from "../../../assets/img/thumbnails/beyond-earth/trending/large.jpg";
 import Icons from "../Icons";
+import { IconsCard } from "../../utils/IconCard";
 
 const CardTrending = (props) => {
+  const iconsCard = IconsCard(props.category, props.isBookmarked);
+
   return (
     <>
       <div className="cardT">
@@ -11,14 +13,19 @@ const CardTrending = (props) => {
           src={props.thumbnail.trending.large}
           alt="card image"
         />
-        <div className="cardT_bookmark">
-          <img src={Icons.BookmarkEmpty} alt="Bookmark" />
+        <div
+          className="cardT_bookmark"
+          onClick={() => {
+            console.log("Bookmark changed");
+          }}
+        >
+          {iconsCard.iconBookmark}
         </div>
         <div className="cardT_info">
           <div className="card_data">
             <p className="card_text">{props.year}</p>
             <div className="card_category">
-              <img className="card_image" src={Icons.NavMovies} alt="Icon" />
+              {iconsCard.iconCategory}
               <p className="card_text">{props.category}</p>
             </div>
             <p className="card_text">{props.rating}</p>
