28c105ec7bb4fbcb155eb7b0c1d10b8b53617b07
SuaferoanTJK <suarezroanfelipe@gmail.com>
2022-04-15 22:45:20 -0500

    Remove unnecesary code

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,34 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import CardTrending from "../components/Cards/CardTrending";
 import Layout from "../layout/Layout";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Mousewheel } from "swiper";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovies } from "../redux/actions/moviesActions";
+import "swiper/css";
 
 const Home = () => {
-  let slides = 1;
-  const [windowWidth, setWindowWidth] = useState(0);
   const dispatch = useDispatch();
   const movies = useSelector((state) => state.movies);
   const trending = useSelector((state) => state.trending);
 
-  const updateDimensions = () => {
-    setWindowWidth(window.innerWidth);
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
-  };
-
   useEffect(() => {
-    updateDimensions();
     dispatch(getMovies());
   }, []);
-
-  if (windowWidth > 550 && windowWidth < 1024) {
-    slides = 2;
-  } else if (windowWidth >= 1024) {
-    slides = 3;
-  }
 
   return (
     <Layout>
@@ -36,8 +22,17 @@ const Home = () => {
         <h2 className="home_title">Trending</h2>
         <div className="home_cards">
           <Swiper
-            slidesPerView={slides}
-            spaceBetween={40}
+            modules={[Mousewheel]}
+            loop={true}
+            mousewheel={true}
+            breakpoints={{
+              550: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
             className="home_swipe"
           >
             {trending.map((movie, index) => (
